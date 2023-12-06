32f0c311338c78c1fc3357e6ed89f54ddc78190f
LivReTaRDIS <lv.zuhause@gmail.com>
2023-12-06 14:29:30 +0100

    Switch From Nova To VSC

--- source/views/cal_week.lua
+++ source/views/cal_week.lua
@@ -2,26 +2,26 @@ local gfx <const> = playdate.graphics
 local ui <const> = playdate.ui
 
 cellWidth = 30
-cellHeight = 30 
+cellHeight = 30
 
 function draw()
-	ui.gridview.new(cellWidth, cellHeight)
+	--ui.gridview.new(cellWidth, cellHeight)
 	gfx.drawText(cellHeight,0,0)
 	gfx.drawText(cellWidth,0,18)
 end
 
 function inputs()
 	if playdate.buttonJustPressed("up") then
-		cellHeight += 0.5
+		cellHeight = cellHeight + 0.5
 	end
 	if playdate.buttonJustPressed("down") then
-		cellHeight -= 0.5
+		cellHeight = cellHeight - 0.5
 	end
 	if playdate.buttonJustPressed("right") then
-		cellHeight += 0.5
+		cellWidth = cellWidth  + 0.5
 	end
 	if playdate.buttonJustPressed("left") then
-		cellWidth -= 0.5
+		cellWidth = cellWidth - 0.5
 	end
 	
 end
@@ -31,6 +31,3 @@ view_1 = {
 	draw = draw,
 	inputs = inputs
 }
-
-
-
